1a49a46b0618186ff9d4e7de986f1d800ddb318a
jessicanordgran <jessicanordgran@mail.weber.edu>
2019-04-03 20:24:57 -0600

    initial submit

--- books/app.py
+++ books/app.py
@@ -1,8 +1,38 @@
+import sqlite3
 # import the Flask class from the flask module
-from flask import Flask, render_template
+from flask import Flask, render_template, g, request, redirect, url_for
+
+PATH = 'db/data.sqlite'
 
 # create the application object
 app = Flask(__name__)
+
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+
+    cursor.close()
+    return results
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
+
 
 # use decorators to link the function to a url
 @app.route('/')
@@ -15,7 +45,9 @@ def index():
 
 @app.route('/history')
 def history():
-    return render_template('history.html')  # render a template
+    history = execute_sql('SELECT history.com_histID, history.com_histLN, history.com_histFN, history.com_histTitle, history.com_histDes FROM history')
+    return render_template('history.html', history=history)  # render a template
+
 @app.route('/h1')
 def h1():
     return render_template('h1.html')  # render a template
@@ -30,7 +62,8 @@ def h3():
 
 @app.route('/fiction')
 def fiction():
-    return render_template('fiction.html')  # render a template
+    fiction = execute_sql('SELECT fiction.com_ficID, fiction.com_ficLN, fiction.com_ficFN, fiction.com_ficTitle, fiction.com_ficDes FROM fiction')
+    return render_template('fiction.html', fiction=fiction)  # render a template
 
 @app.route('/f1')
 def f1():
@@ -47,7 +80,8 @@ def f3():
 
 @app.route('/scienceFiction')
 def scienceFiction():
-    return render_template('scienceFiction.html')  # render a template
+        scienceFiction = execute_sql('SELECT scienceFiction.com_scifiID, scienceFiction.com_scifiLN, scienceFiction.com_scifiFN, scienceFiction.com_scifiTitle, scienceFiction.com_scifiDes FROM scienceFiction')
+    return render_template('scienceFiction.html', sciencFiction=scienceFiction)  # render a template
 
 @app.route('/sF1')
 def sF1():
--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Owner\Desktop\Classes\WEB3200\as06_bookReview1\venv"
+$env:VIRTUAL_ENV="C:\Users\Owner\Desktop\Classes\WEB3200\as06_bookReview2\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
